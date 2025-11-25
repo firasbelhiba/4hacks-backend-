48836baa09d11eba8ee67a0b1273d770d98336c5
Ayoub Amer <ayouub.ameur@gmail.com>
2025-11-25 11:30:04 +0100

    feat: add create hackathon enpoint

--- src/hackathon/hackathon.module.ts
+++ src/hackathon/hackathon.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HackathonService } from './hackathon.service';
 import { HackathonController } from './hackathon.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [HackathonService],
   controllers: [HackathonController]
 })
