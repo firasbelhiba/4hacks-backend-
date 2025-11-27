a3142b21647e64d783eb124bbc10c525e5062d35
Ayoub Amer <ayouub.ameur@gmail.com>
2025-11-27 09:44:29 +0100

    feat: init hackathon-request module

--- src/hackathon/hackathon.module.ts
+++ src/hackathon/hackathon.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HackathonService } from './hackathon.service';
 import { HackathonController } from './hackathon.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { RequestModule } from './request/request.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, RequestModule],
   providers: [HackathonService],
   controllers: [HackathonController]
 })
