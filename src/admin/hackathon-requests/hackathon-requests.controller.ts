b9cc810de200799eb10a22b157e40d2a4b0cc623
Ayoub Amer <ayouub.ameur@gmail.com>
2025-11-27 13:39:43 +0100

    feat: add get all hackathon-requests for admin

--- src/admin/hackathon-requests/hackathon-requests.controller.ts
+++ src/admin/hackathon-requests/hackathon-requests.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiInternalServerErrorResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { HackathonRequestsService } from './hackathon-requests.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { RolesGuard } from '../guards/roles.guard';
+import { Roles } from '../decorators/roles.decorator';
+import { UserRole } from 'generated/prisma';
 
-@Controller('hackathon-requests')
-export class HackathonRequestsController {}
+@ApiTags('Hackathon Requests')
+// Apply the JWT Guard and Roles Guard and check for ADMIN role globally for this controller
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(UserRole.ADMIN)
+@Controller('admin/hackathon-requests')
+export class HackathonRequestsController {
+  constructor(
+    private readonly hackathonRequestsService: HackathonRequestsService,
+  ) {}
+
+  @ApiOperation({
+    summary: 'Get all hackathon requests',
+    description:
+      'Get all hackathon requests. This endpoint can be accessed by ADMIN role only',
+  })
+  @ApiResponse({ status: 200, description: 'List of hackathon requests' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized access' })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
+  @ApiForbiddenResponse({ description: 'Forbidden access' })
+  @ApiBearerAuth()
+  @Get()
+  async findAll() {
+    return await this.hackathonRequestsService.findAll();
+  }
+}
--- src/admin/hackathon-requests/hackathon-requests.service.ts
+++ src/admin/hackathon-requests/hackathon-requests.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class HackathonRequestsService {}
+export class HackathonRequestsService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll() {
+    return await this.prisma.hackathonCreationRequest.findMany({
+      include: {
+        organization: {
+          select: {
+            id: true,
+            name: true,
+            slug: true,
+            logo: true,
+            owner: {
+              select: {
+                id: true,
+                name: true,
+                username: true,
+                email: true,
+                role: true,
+              },
+            },
+          },
+        },
+        hackathon: true,
+        approvedBy: {
+          select: {
+            id: true,
+            name: true,
+            username: true,
+            email: true,
+            role: true,
+          },
+        },
+        rejectedBy: {
+          select: {
+            id: true,
+            name: true,
+            username: true,
+            email: true,
+            role: true,
+          },
+        },
+      },
+    });
+  }
+}
