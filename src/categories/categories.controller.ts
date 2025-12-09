491949af31737face35b10bd76971b4070a36fc1
Ayoub Amer <ayouub.ameur@gmail.com>
2025-12-09 07:16:04 +0100

    feat: add update, delete a hackathon category

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import {
   ApiBearerAuth,
   ApiConflictResponse,
   ApiForbiddenResponse,
+  ApiNotFoundResponse,
   ApiOperation,
   ApiResponse,
   ApiTags,
@@ -14,6 +24,7 @@ import { RolesGuard } from 'src/admin/guards/roles.guard';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { Roles } from 'src/admin/decorators/roles.decorator';
 import { UserRole } from '@prisma/client';
+import { UpdateCategoryDto } from './dto/update.dto';
 
 @ApiTags('Categories')
 @Controller('categories')
@@ -21,7 +32,26 @@ export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @ApiOperation({ summary: 'Find all categories' })
-  @ApiResponse({ status: 200, description: 'List of categories' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of categories',
+    example: [
+      {
+        id: 'cmiy67mia0002osfdlczmv4i6',
+        name: 'WEB3',
+        description: 'All web3 products',
+        createdAt: '2025-12-09T05:59:35.602Z',
+        updatedAt: '2025-12-09T05:59:35.602Z',
+      },
+      {
+        id: 'cmiy68du50003osfdrafk0k06',
+        name: 'AI',
+        description: 'All AI hackathons',
+        createdAt: '2025-12-09T06:00:11.021Z',
+        updatedAt: '2025-12-09T06:00:11.021Z',
+      },
+    ],
+  })
   @ApiResponse({ status: 404, description: 'Categories not found' })
   @Get()
   async findAll() {
@@ -33,15 +63,100 @@ export class CategoriesController {
     description:
       'Add new category. This endpoint is protected by the @Admin decorator.',
   })
-  @ApiResponse({ status: 201, description: 'Category created' })
+  @ApiResponse({
+    status: 201,
+    description: 'Category created',
+    example: {
+      message: 'Category created successfully',
+      data: {
+        id: 'cmiy6lnt00000gofd1djj22k1',
+        name: 'TEST2',
+        description: 'All test products',
+        createdAt: '2025-12-09T06:10:30.468Z',
+        updatedAt: '2025-12-09T06:10:30.468Z',
+      },
+    },
+  })
   @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   @ApiForbiddenResponse({ description: 'Forbidden' })
-  @ApiConflictResponse({ description: 'Category already exists' })
+  @ApiConflictResponse({
+    description: 'Category already exists',
+    example: {
+      message: 'Category already exists',
+      error: 'Conflict',
+      statusCode: 409,
+    },
+  })
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.ADMIN)
   @Post()
   async create(@Body() createCategoryDto: CreateCategoryDto) {
     return await this.categoriesService.create(createCategoryDto);
+  }
+
+  @ApiOperation({
+    summary: 'Update category',
+    description:
+      'Update category. This endpoint is protected by the @Admin decorator.',
+  })
+  @ApiResponse({ status: 200, description: 'Category updated' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiConflictResponse({
+    description: 'Category with this name already exists',
+    example: {
+      message: 'Category with this name already exists',
+      error: 'Conflict',
+      statusCode: 409,
+    },
+  })
+  @ApiNotFoundResponse({
+    description: 'Category not found',
+    example: {
+      message: 'Category not found',
+      error: 'Not Found',
+      statusCode: 404,
+    },
+  })
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    return await this.categoriesService.update(id, updateCategoryDto);
+  }
+
+  @ApiOperation({
+    summary: 'Delete category',
+    description:
+      'Delete category. This endpoint is protected by the @Admin decorator.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Category deleted',
+    example: {
+      message: 'Category deleted successfully',
+      data: {
+        id: 'cmiy6lnt00000gofd1djj22k1',
+        name: 'TEST2',
+        description: 'All test products',
+        createdAt: '2025-12-09T06:10:30.468Z',
+        updatedAt: '2025-12-09T06:10:30.468Z',
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiNotFoundResponse({ description: 'Category not found' })
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return await this.categoriesService.delete(id);
   }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,8 +1,13 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Logger } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCategoryDto } from './dto/create.dto';
 import type { UserMin } from 'src/common/types';
+import { UpdateCategoryDto } from './dto/update.dto';
 
 @Injectable()
 export class CategoriesService {
@@ -28,11 +33,92 @@ export class CategoriesService {
       throw new ConflictException('Category already exists');
     }
 
-    return await this.prismaService.hackathonCategory.create({
+    const newCategory = await this.prismaService.hackathonCategory.create({
       data: {
         name: name.toUpperCase(),
         description,
       },
     });
+
+    return {
+      message: 'Category created successfully',
+      data: newCategory,
+    };
+  }
+
+  async update(id: string, updateCategoryDto: UpdateCategoryDto) {
+    const { name, description } = updateCategoryDto;
+
+    // Find if category already exists
+    const category = await this.prismaService.hackathonCategory.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!category) {
+      throw new NotFoundException('Category not found');
+    }
+
+    if (name) {
+      category.name = name.toUpperCase();
+
+      // Find if category already exists
+      const existingCategory =
+        await this.prismaService.hackathonCategory.findFirst({
+          where: {
+            name: name.toUpperCase(),
+            id: {
+              not: id,
+            },
+          },
+        });
+
+      if (existingCategory) {
+        throw new ConflictException('Category with this name already exists');
+      }
+    }
+
+    if (description) {
+      category.description = description;
+    }
+
+    const updatedCategory = await this.prismaService.hackathonCategory.update({
+      where: {
+        id,
+      },
+      data: {
+        name: category.name,
+        description: category.description,
+      },
+    });
+
+    return {
+      message: 'Category updated successfully',
+      data: updatedCategory,
+    };
+  }
+
+  async delete(id: string) {
+    const category = await this.prismaService.hackathonCategory.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!category) {
+      throw new NotFoundException('Category not found');
+    }
+
+    const deletedCategory = await this.prismaService.hackathonCategory.delete({
+      where: {
+        id,
+      },
+    });
+
+    return {
+      message: 'Category deleted successfully',
+      data: deletedCategory,
+    };
   }
 }
